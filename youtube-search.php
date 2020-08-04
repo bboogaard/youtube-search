3260161cf7a5edf4f46c166102174e8bb2f7ff0f
bboogaard <padawan@hetnet.nl>
2020-08-04 14:10:21 +0200

    Update release.txt/Bump version

--- youtube-search.php
+++ youtube-search.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.wp-wikkel.nl/
  * Text Domain:     youtube-search
  * Domain Path:     /languages
- * Version:         1.0.0
+ * Version:         1.1.0
  *
  * @package         Youtube Search
  */
